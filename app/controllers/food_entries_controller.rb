8d63c2a2e344d56dfd50b9c31098ef0fda7d6943
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2026-06-03 18:33:30 -0400

    Make Turbo Stuff Work Properly

--- app/controllers/food_entries_controller.rb
+++ app/controllers/food_entries_controller.rb
@@ -4,7 +4,11 @@ class FoodEntriesController < ApplicationController
   def create
     @food_entry = Current.user.food_entries.build(food_entry_params)
     if @food_entry.save
-      redirect_to day_path_for(@food_entry)
+      @day_presenter = DayPresenter.new(user: Current.user, date: @food_entry.date)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to day_path_for(@food_entry) }
+      end
     else
       redirect_to day_path_for(Date.parse(params[:food_entry][:date])), alert: @food_entry.errors.full_messages.to_sentence
     end
@@ -14,7 +18,11 @@ class FoodEntriesController < ApplicationController
 
   def update
     if @food_entry.update(food_entry_params)
-      redirect_to day_path_for(@food_entry)
+      @day_presenter = DayPresenter.new(user: Current.user, date: @food_entry.date)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to day_path_for(@food_entry) }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -23,8 +31,12 @@ class FoodEntriesController < ApplicationController
   def destroy
     @date = @food_entry.date
     @food_entry.destroy!
+    @day_presenter = DayPresenter.new(user: Current.user, date: @food_entry.date)
 
-    redirect_to day_path_for(@date)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to day_path_for(@date) }
+    end
   end
 
   private
---
+++ app/presenters/day_presenter.rb
@@ -0,0 +1,56 @@
+class DayPresenter
+  attr_reader :user, :date
+
+  def initialize(user:, date:)
+    @user = user
+    @date = date
+  end
+
+  def prev_day
+    date - 1.day
+  end
+
+  def next_day
+    date + 1.day
+  end
+
+  def today?
+    date == Date.today
+  end
+
+  def total_calories
+    food_entries.sum(:calories)
+  end
+
+  def calories_remaining
+    daily_calorie_goal - total_calories
+  end
+
+  def daily_calorie_goal
+    user.daily_calorie_goal
+  end
+
+  def new_food_entry
+    @new_food_entry ||= user.food_entries.build(date: date)
+  end
+
+  def new_weight_entry
+    @new_weight_entry ||= user.weight_entries.build(date: date)
+  end
+
+  def last_weight_entry
+    weight_entries.max_by(&:date)
+  end
+
+  def weight_entry
+    weight_entries.find_by(date: date)
+  end
+
+  def food_entries
+    @food_entries ||= user.food_entries.where(date: date).order(:created_at)
+  end
+
+  def weight_entries
+    @weight_entries ||= user.weight_entries
+  end
+end
