ba426760de26389caa2d7ed1293843262ea4f7ab
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2026-06-03 22:03:48 -0400

    Make Turbo Stuff Work Properly (#5)
    * Make Turbo Stuff Work Properly
    
    * Extract a Helper

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,14 +1,6 @@
 class DaysController < ApplicationController
   def show
-    @date              = parse_date
-    @food_entries      = Current.user.food_entries.where(date: @date).order(:created_at)
-    @total_calories    = @food_entries.sum(:calories)
-    @daily_calorie_goal = Current.user.daily_calorie_goal
-    @calories_remaining = @daily_calorie_goal - @total_calories
-    @last_weight_entry = Current.user.weight_entries.max_by(&:date)
-    @weight_entry      = Current.user.weight_entries.find_by(date: @date)
-    @new_food_entry    = Current.user.food_entries.build(date: @date)
-    @new_weight_entry  = Current.user.weight_entries.build(date: @date)
+    @day_presenter = ::DayPresenter.new(user: Current.user, date: parse_date)
   end
 
   private
