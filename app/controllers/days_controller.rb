b78a488fde2da24fe2a50ce866d44b944e418b59
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2026-06-06 17:01:25 -0400

    Refactor DaysController

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,16 +1,16 @@
 class DaysController < ApplicationController
+  before_action :validate_date
+
   def show
-    @day_presenter = ::DayPresenter.new(user: Current.user, date: parse_date)
+    @day_presenter = ::DayPresenter.new(user: Current.user, date: @date)
   end
 
   private
 
-  def parse_date
-    requested_date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
-    raise Date::Error if requested_date > Date.today
-
-    requested_date
+  def validate_date
+    @date = Date.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+    raise Date::Error if @date > Date.today
   rescue Date::Error
-    redirect_to(day_path_for(Date.today), alert: "Invalid day!") and return
+    redirect_to day_path_for(Date.today), alert: "Invalid day!"
   end
 end
