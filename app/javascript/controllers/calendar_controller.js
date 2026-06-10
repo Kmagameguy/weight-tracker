7e1e4799b09c0fdb658ab7202c2a00c071e3fc5b
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2026-06-09 21:29:14 -0400

    Adds Calendar-Based Navigation (#14)
    * Initial Pass at a Calendar View
    
    * Switch Calendar to Sliding Tray
    
    * Add CalendarsController Tests
    
    * Remove Redundant Today Link in Header
    
    * Refactor to Use Dedicated Css Styling & a Calendar Presenter

---
+++ app/javascript/controllers/calendar_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["tray"]
+
+  connect() {
+    this.isOpen = false
+  }
+
+  toggle(event) {
+    event.stopPropagation()
+    this.isOpen ? this.close() : this.open()
+  }
+
+  open() {
+    this.trayTarget.style.gridTemplateRows = "1fr"
+    this.trayTarget.style.opacity = "1"
+    this.trayTarget.style.transform = "translateY(0)"
+    this.isOpen = true
+  }
+
+  close() {
+    this.trayTarget.style.gridTemplateRows = "0fr"
+    this.trayTarget.style.opacity = "0"
+    this.trayTarget.style.transform = "translateY(-8px)"
+    this.isOpen = false
+  }
+}
